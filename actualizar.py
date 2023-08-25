171741f17fd2e741fc5b6c7f9272e2bddd183c17
broly1987 <arielcamilotrabajo@gmail.com>
2023-08-24 23:18:19 -0300

    otra actualizacion hoy 24

--- actualizar.py
+++ actualizar.py
@@ -4,14 +4,14 @@ import subprocess
 class Actualizador:
     @staticmethod
     def hay_actualizacion_disponible():
-        url_actualizacion = "https://raw.githubusercontent.com/broly1987/meter/main/act.json?token=GHSAT0AAAAAACGJ7OV737SP7TONKSB2R4NYZHIBTMQ"
+        url_actualizacion = "https://raw.githubusercontent.com/broly1987/meter/main/act.json"
 
         try:
             response = requests.get(url_actualizacion)
             if response.status_code == 200:
                 return True
         except requests.exceptions.RequestException as e:
-            print("Error cheking update:", str(e))
+            print("Error checking update:", str(e))
 
         return False
 
@@ -23,6 +23,14 @@ class Actualizador:
         try:
             subprocess.run(comando_establecer_rama, shell=True, check=True)
             subprocess.run(comando_actualizacion, shell=True, check=True)
-            print("update succefull.")
+            print("Update successful.")
         except subprocess.CalledProcessError as e:
-            print("Error updating :", str(e))
+            print("Error updating:", str(e))
+
+# Llamar a los métodos de la clase Actualizador
+if Actualizador.hay_actualizacion_disponible():
+    respuesta = input("Hay una actualización disponible. ¿Deseas actualizar? (s/n): ")
+    if respuesta.lower() == "s":
+        Actualizador.realizar_actualizacion()
+    else:
+        print("Actualización cancelada.")
